dd149d19c2a5d4a23e9427ef84a6552b0d31d029
Alex <alexhowan@hotmail.com>
2019-01-16 00:25:51 +0100

    -

---
+++ app/Models/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Http\Traits\TimestampsTrait;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Task extends Model
+{
+    use TimestampsTrait;
+    // Define the table
+    protected $table = 'tasks';
+
+    // created at (5 minutes ago)
+
+    // update at
+}
